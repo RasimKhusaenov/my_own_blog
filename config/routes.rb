abbd29421ed407ea115d7fcd6cfa5e18ad6e3aeb
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-04-08 13:27:25 +0300

    Change user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get "blog", to: "blog#index"
 
-  get "/users", to: "users#show"
-  resources :users, only: %i[new create]
+  resource :user, only: %i[new create show]
   resource :session, only: %i[new create destroy]
 end
