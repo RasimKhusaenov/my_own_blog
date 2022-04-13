b443c2c399a3c56056541804d8dc33e38cca3610
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-04-13 21:53:12 +0300

    Add role to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  extend Enumerize
+
   has_secure_password
+
+  enumerize :role, in: %i[reader admin superadmin], scope: :shallow, predicates: true
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
+  validates :role, presence: true
+
+  def administrative_role?
+    admin? || superadmin?
+  end
 end
