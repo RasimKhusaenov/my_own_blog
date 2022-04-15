b735b639c7d1fb7f0f275f4c9d2f65ba87c418e1
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-04-15 17:21:41 +0300

    Optimize admin shared context

--- spec/support/shared_context/when_admin_signed_in.rb
+++ spec/support/shared_context/when_admin_signed_in.rb
@@ -1,14 +1,5 @@
 RSpec.shared_context "when admin signed in" do
-  let(:current_user_attributes) { attributes_for(:user) }
+  include_context "when user signed in"
 
-  before do
-    create(:user, :admin, **current_user_attributes)
-
-    visit new_session_path
-
-    fill_in "Email", with: current_user_attributes[:email]
-    fill_in "Password", with: current_user_attributes[:password]
-
-    click_button "Sign In"
-  end
+  let(:role) { :admin }
 end
--- spec/support/shared_context/when_user_signed_in.rb
+++ spec/support/shared_context/when_user_signed_in.rb
@@ -1,13 +1,12 @@
 RSpec.shared_context "when user signed in" do
-  let(:current_user_attributes) { attributes_for(:user) }
+  let(:current_user) { create(:user, first_name: "John", email: "john.smith@test.com", password: "pwd", role: role) }
+  let(:role) { :reader }
 
   before do
-    create(:user, **current_user_attributes)
-
     visit new_session_path
 
-    fill_in "Email", with: current_user_attributes[:email]
-    fill_in "Password", with: current_user_attributes[:password]
+    fill_in "Email", with: current_user.email
+    fill_in "Password", with: current_user.password
 
     click_button "Sign In"
   end
