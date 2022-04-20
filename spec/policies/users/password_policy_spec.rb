22aceee3721b98e62c1d677ae6507c2779818a70
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-04-20 17:49:26 +0300

    Add test for PasswordPolicy

--- spec/policies/users/password_policy_spec.rb
+++ spec/policies/users/password_policy_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
-RSpec.describe Users::MePolicy do
+RSpec.describe Users::PasswordPolicy do
   let(:policy) { described_class.new(user: current_user) }
   let(:current_user) { build_stubbed(:user, :reader) }
 
-  %i[show? edit? update?].each do |policy_method|
+  %i[edit? update?].each do |policy_method|
     describe "##{policy_method}" do
       subject { policy.apply(policy_method) }
 
