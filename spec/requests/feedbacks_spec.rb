8d13465130e0c0994a7700ede2542c5da9cbc378
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-05-10 20:26:39 +0300

    Add red scenario

--- spec/requests/feedbacks_spec.rb
+++ spec/requests/feedbacks_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Feedback", type: :request do
   let(:valid_attributes) { attributes_for(:feedback) }
+  let(:invalid_attributes) { attributes_for(:feedback, email: "not_correct") }
 
   describe "GET /new" do
     it "renders a successful response" do
@@ -20,6 +21,20 @@ RSpec.describe "Feedback", type: :request do
 
       it "redirects to the blog page" do
         expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with invalid parameters" do
+      before do
+        post feedback_path, params: { feedback: invalid_attributes }
+      end
+
+      it "renders a successful response" do
+        expect(response).to be_successful
+      end
+
+      it "doesn't redirect" do
+        expect(response).not_to have_http_status(:redirect)
       end
     end
   end
