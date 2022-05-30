4cc26ffd7c0bad9b33a8c807a1ca7d15e3053aec
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-05-30 16:45:48 +0300

    Move roles to const

--- app/models/company_member.rb
+++ app/models/company_member.rb
@@ -1,10 +1,12 @@
 class CompanyMember < ApplicationRecord
   extend Enumerize
 
+  ROLES = %i[owner writer].freeze
+
   belongs_to :company
   belongs_to :user
 
-  enumerize :role, in: %i[owner writer], scope: true
+  enumerize :role, in: ROLES, scope: true
 
   validates :role, presence: true
   validates :user_id, uniqueness: { scope: :company_id }
--- spec/factories/company_members.rb
+++ spec/factories/company_members.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :company_member do
     company
     user
-    role { CompanyMember.role.values.sample }
+    role { CompanyMember::ROLES.sample }
   end
 end
