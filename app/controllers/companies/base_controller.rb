8609fc667ba30abaac5f7dab33c0fee76daa3466
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-06-12 11:03:58 +0300

    Remove redundant

--- app/controllers/companies/base_controller.rb
+++ app/controllers/companies/base_controller.rb
@@ -3,7 +3,7 @@ module Companies
     include Authentication
     include Authorization
 
-    before_action :authenticate_company!
+    # before_action :authenticate_company!
     before_action :authorize_resource!
   end
 end
