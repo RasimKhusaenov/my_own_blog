552d02f5681b6bf31b872b388351b612644131b3
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-06-27 23:31:28 +0300

    Refactor js script

--- app/javascript/packs/textEditing.js
+++ app/javascript/packs/textEditing.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll('[data-behavior=auto-resize]').forEach((target) => {
-    target.style.height = target.scrollHeight + 'px';
-    target.style['overflow-y'] = 'hidden';
-
     target.addEventListener('input', (event) => {
-    	event.target.style.height = 'auto';
       event.target.style.height = event.target.scrollHeight + 'px';
     });
   });
