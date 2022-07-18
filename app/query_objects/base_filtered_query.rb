9714d90cb46cc4d9154af94d5e036a0e971ba84d
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-07-18 17:42:21 +0300

    Upgrade `BaseFilteredQuery`

--- app/query_objects/base_filtered_query.rb
+++ app/query_objects/base_filtered_query.rb
@@ -1,16 +1,28 @@
 class BaseFilteredQuery
-  def initialize(relation, filter_params = {})
+  def initialize(relation, options = {})
     @relation = relation
-    @filter_params = filter_params
+    @filter_params = sanitized_filter_params(options)
   end
 
   def all
     filter_params.reduce(relation) do |relation, (key, value)|
-      public_send("by_#{key}", relation, value)
+      method_name = "by_#{key}"
+      if self.class.private_method_defined?(method_name)
+        send(method_name, relation, value)
+      else
+        relation
+      end
     end
   end
 
   private
+
+  def sanitized_filter_params(params)
+    params
+      .to_h
+      .deep_symbolize_keys
+      .reject { |_, v| v.to_s.blank? }
+  end
 
   attr_reader :relation, :filter_params
 end
