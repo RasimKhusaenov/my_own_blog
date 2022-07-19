7d83a0df12f484e9a8cf9ff97ec1732cfa889c7e
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-07-19 13:01:16 +0300

    Apply review comments

--- spec/features/articles/search_spec.rb
+++ spec/features/articles/search_spec.rb
@@ -1,12 +1,10 @@
 require "rails_helper"
 
 RSpec.feature "Search Article" do
-  let!(:target_articles) do
-    [
-      create(:article, :published, title: "How to add search to Rails project?"),
-      create(:article, :published, content: "Why you not use pg_search?")
-    ]
-  end
+  let!(:article_1) { create(:article, :published, title: "How to add search to Rails project?") }
+  let!(:article_2) { create(:article, :published, content: "Why you not use pg_search?") }
+  let!(:article_3) { create(:article, :published, title: "Invalid") }
+  let!(:article_4) { create(:article, content: "Search") }
 
   scenario "User search article" do
     visit root_path
@@ -15,7 +13,10 @@ RSpec.feature "Search Article" do
 
     click_button "Search"
 
-    expect(page).to have_content(target_articles.first.title)
-    expect(page).to have_content(target_articles.last.content)
+    expect(page).to have_content(article_1.title)
+    expect(page).to have_content(article_2.title)
+
+    expect(page).not_to have_content(article_3.title)
+    expect(page).not_to have_content(article_4.title)
   end
 end
--- spec/query_objects/filtered_articles_query_spec.rb
+++ spec/query_objects/filtered_articles_query_spec.rb
@@ -12,19 +12,23 @@ RSpec.describe FilteredArticlesQuery do
     it { is_expected.to match_array(relation) }
   end
 
-  describe "search param" do
-    let(:filter_params) { { search: "search" } }
-    let!(:target_articles) do
-      [
-        create(:article, title: "How to add search to Rails project?"),
-        create(:article, content: "Why you not use pg_search?")
-      ]
+  describe ".by_search" do
+    let!(:article_1) { create :article, :published, title: "How to add search to Rails project?" }
+    let!(:article_2) { create :article, content: "Why you not use pg_search?" }
+    let!(:article_3) { create :article, title: "Invalid" }
+
+    context "when search param is empty" do
+      let(:filter_params) { { search: "" } }
+      let(:expected_result) { [article_1, article_2, article_3] }
+
+      it { is_expected.to match_array(expected_result) }
     end
 
-    before do
-      create :article, :published
-    end
+    context "when search param is present" do
+      let(:filter_params) { { search: "search" } }
+      let(:expected_result) { [article_1, article_2] }
 
-    it { is_expected.to match_array(target_articles) }
+      it { is_expected.to match_array(expected_result) }
+    end
   end
 end
