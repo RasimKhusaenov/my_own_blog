95c8015614c25e18cf8ba5a1db9e05f8eb653ebe
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-08-13 14:26:32 +0300

    Fix N+1 and new article creating

--- app/controllers/users/articles_controller.rb
+++ app/controllers/users/articles_controller.rb
@@ -1,8 +1,8 @@
 module Users
   class ArticlesController < Users::BaseController
-    expose :article, scope: -> { authorized_articles }
+    expose :article, -> { fetch_article }
     expose :articles, -> { ArticleDecorator.wrap(paginate(authorized_articles)) }
-    expose :company, -> { Company.find_by!(slug: request.subdomain) }
+    expose :company, -> { Company.find_by(slug: request.subdomain) }
 
     def new; end
 
@@ -21,11 +21,31 @@ module Users
     end
 
     def authorized_articles
-      authorized(Article.all)
+      authorized(Article.all.includes(:comments))
     end
 
     def article_params
       params.require(:article).permit(:title, :content).merge(user: current_user)
+    end
+
+    def fetch_article
+      if params[:id].present?
+        Article.find(params[:id])
+      elsif company.present?
+        fetch_company_article
+      elsif params[:article].present?
+        Article.new(article_params)
+      else
+        Article.new
+      end
+    end
+
+    def fetch_company_article
+      if params[:article].present?
+        company.articles.build(article_params)
+      else
+        company.articles.build
+      end
     end
   end
 end
