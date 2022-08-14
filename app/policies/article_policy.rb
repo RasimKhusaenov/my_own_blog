352bc0c0262d20cb285c7ebb5a2568d4f8c9feb9
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-08-14 10:38:09 +0300

    Add ability to readers view their own drafts

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -2,7 +2,7 @@ class ArticlePolicy < ApplicationPolicy
   relation_scope do |relation|
     next relation if user&.administrative_role?
 
-    relation.published
+    relation.published && relation.where(user: user)
     # TODO: Find out how to show current company drafts to company members
   end
 
