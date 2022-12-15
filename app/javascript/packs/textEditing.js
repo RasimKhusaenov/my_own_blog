4f340fadff38ac06f334debfc2f2afca42601694
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-12-15 21:20:15 +0300

    Fix bug with auto-resizing

--- app/javascript/packs/textEditing.js
+++ app/javascript/packs/textEditing.js
@@ -7,3 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// Needed for auto-resizing loaded edit form
+// TODO: Refactor it with Stimulus
+document.addEventListener("turbo:frame-render", () => {
+  document.querySelectorAll('[data-behavior=auto-resize]').forEach((target) => {
+    target.style.height = target.scrollHeight + 'px';
+    target.addEventListener('input', (event) => {
+      event.target.style.height = 'auto';
+      event.target.style.height = event.target.scrollHeight + 'px';
+    });
+  });
+});
