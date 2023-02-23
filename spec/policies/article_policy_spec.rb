eb9a9b7697965a6c5aa504606772dc7aedcdcf8b
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2023-02-23 22:40:15 +0300

    Add ability to update any article by company owner

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -11,35 +11,30 @@ RSpec.describe ArticlePolicy do
     end
   end
 
-  describe "#update?" do
-    subject { policy.apply(:update?) }
+  describe "#edit?" do
+    subject { policy.apply(:edit?) }
 
-    let(:record) { build :article }
+    let(:current_user) { create :user, :reader }
 
-    context "when current user is superadmin" do
-      let(:current_user) { build :user, :superadmin }
+    it_behaves_like "administrative role rule" do
+      let(:record) { build_stubbed :article }
+    end
+
+    context "when current user is author of the article" do
+      let(:record) { build :article, user: current_user }
 
       it { is_expected.to be true }
     end
 
-    context "when current user is admin" do
-      let(:current_user) { build :user, :admin }
+    context "when current user is owner of the article's company" do
+      let(:record) { build_stubbed :article, company: company }
+      let(:company) { create :company }
+
+      before do
+        create :company_member, user: current_user, company: company
+      end
 
       it { is_expected.to be true }
-    end
-
-    context "when current user is reader" do
-      let(:current_user) { build :user, :reader }
-
-      context "when he isn't author of the article" do
-        it { is_expected.to be false }
-      end
-
-      context "when he is author of the article" do
-        let(:record) { build :article, user: current_user }
-
-        it { is_expected.to be true }
-      end
     end
   end
 end
